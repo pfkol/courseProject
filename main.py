c7341cfd0bc6bbb58f07f8fdffb0c46b895b5d90
piotr <pfkol@poczta.onet.pl>
2020-11-05 13:41:19 +0100

    commit third line pronted

--- main.py
+++ main.py
@@ -13,5 +13,6 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('Piotrek - update on github')
     print_hi('second line pronted')
+    print_hi('third line printed')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
