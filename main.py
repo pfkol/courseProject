8394cad41a41642b7f5ac1e52bfdb03b293c0c0b
piotr <pfkol@poczta.onet.pl>
2020-12-29 19:26:55 +0100

    removed line print thirt line

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('Piotrek - update on github')
     print_hi('second line printed')
-    print_hi('third line printed')
     jsonObj = JSONTools.Json()
     jsonObj.getJson()
     data = jsonObj.getDataFromJson()
